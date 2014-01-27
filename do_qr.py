ca351efbf5708a445e7a95809e7ef36afa1f19f6
Kim SJ <kim@spencejones.com>
2014-01-27 23:04:58 +0000

    do_qr.py apt-get instuctions modified

--- do_qr.py
+++ do_qr.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python
 #
 # to make this work:
-#    sudo apt-get install libcups2-dev
+#    sudo apt-get install libcups2-dev python-dev
 #    (sudo) pip install qrcode pillow pycups
 #    
 # label size 36 x 89 mm @ 300 dpi
